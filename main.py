5a5500656bc2dbb2ba7754dd87ee0304076d7f01
cmghoglund <cmghoglund@gmail.com>
2023-05-14 12:47:20 +0900

    Defined function 'print_list' to print list in specified format.

--- main.py
+++ main.py
@@ -2,16 +2,21 @@
 
 import random
 
+def print_list(target_list): # Neatly format printed list
+    for item in target_list:
+        print(item, end=" ")
+    print()
+
 def pop_random(target_list=None):
     if target_list is None: # If no list provided as argument, use default list created by program
         target_list = list(range(1, 11)) # Create default list with integers 1-10
 
-    print(target_list) # Print original list
+    print_list(target_list) # Print original list in specified format
 
     for _ in range(len(target_list)): # Loop through all numbers in list
         pop_index = random.randint(0, len(target_list) - 1) # Choose random number from list
         target_list.pop(pop_index) # Pop the number
-        print(target_list) # Print the resulting list
+        print_list(target_list) # Print the resulting list in specified format
 
 if __name__ == '__main__':
     pop_random()
